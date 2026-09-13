ae7988e47e117fd26fd8570b5befe13141435b50
Mustapha Amin <mustaphaamin2003@gmail.com>
2026-09-13 17:20:33 +0100

    refactor: update baseurl

--- lib/api/endpoints.dart
+++ lib/api/endpoints.dart
@@ -1,6 +1,6 @@
 abstract class Endpoints {
   static const baseUrl =
-      "https://crudcrud.com/api/420eb8b4d4434e49ac55908e6bdf28e1";
+      "https://crudcrud.com/api/325547117a6d4e519e97fb394e7b35d5";
   static const expenses = "/expenses";
   static String expenseByID(String id) => "/expenses/$id";
 }
