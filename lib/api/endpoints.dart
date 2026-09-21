3b143441271b7a7fd77e2b0756739833071388f0
Mustapha Amin <mustaphaamin2003@gmail.com>
2026-09-21 11:26:07 +0100

    chore: update baseUrl

--- lib/api/endpoints.dart
+++ lib/api/endpoints.dart
@@ -1,6 +1,6 @@
 abstract class Endpoints {
   static const baseUrl =
-      "https://crudcrud.com/api/325547117a6d4e519e97fb394e7b35d5";
+      "https://crudcrud.com/api/773dff8c390c4fa4a761d62492a6f709";
   static const expenses = "/expenses";
   static String expenseByID(String id) => "/expenses/$id";
 }
